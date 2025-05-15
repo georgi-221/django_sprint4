89c27f87f08fc940f98d755b3a67854b72d97ca2
georgi-221 <goshan@gmail.com>
2025-05-15 19:38:57 +0300

    fix

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -27,14 +27,14 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
 
     path(
-            'auth/registration/',
-            CreateView.as_view(
-                template_name='registration/registration_form.html',
-                form_class=UserCreationForm,
-                success_url= reverse_lazy('blog: index'),
-            ),
-            name='registration',
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url= reverse_lazy('blog: index'),
         ),
+        name='registration',
+    ),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'pages.views.page_not_found'
 handler500 = 'pages.views.custom_500'
